d2c221d3f5d7d9f98111817b94e919eaf8daa130
Kerri Kuhnke <kerkuh7439@students.ecpi.edu>
2026-09-06 14:51:05 -0400

    Add files via upload

---
+++ Kuhnke_Wk1A2/index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Welcome to PHP!</title>
+</head>
+
+<body>
+    <?php echo '<h1 style="text-align: center;">Welcome to PHP!</h1>'?>
+    <?php echo '<h2 style="text-align: center;">My Name is: Kerri Kuhnke.</h2>' ?>
+    <?php echo '<p style="text-align: center;">The Date is: September 6, 2026.</p>' ?>
+    <?php echo '<p style="text-align: center;">The Time is: 1435</p>' ?>
+    <p>This line was added using Visual Studio Code.</p>
+</body>
+
+</html>
